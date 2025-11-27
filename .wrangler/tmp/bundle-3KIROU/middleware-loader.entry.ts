5be9e1f73e078f7e4650dc8b15f6d4a480d470c5
lol <lol@loldeMacBook-Air.local>
2025-11-27 17:54:37 +0800

    change to two columns and add dark mode

--- .wrangler/tmp/bundle-3KIROU/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-3KIROU/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/lol/coding/dutyboard/.wrangler/tmp/bundle-9ekbl3/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/lol/coding/dutyboard/.wrangler/tmp/bundle-3KIROU/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/lol/coding/dutyboard/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/lol/coding/dutyboard/.wrangler/tmp/bundle-9ekbl3/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/lol/coding/dutyboard/.wrangler/tmp/bundle-3KIROU/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/lol/coding/dutyboard/.wrangler/tmp/bundle-9ekbl3/middleware-insertion-facade.js";
+export * from "/Users/lol/coding/dutyboard/.wrangler/tmp/bundle-3KIROU/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
